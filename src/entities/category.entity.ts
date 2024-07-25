dfe9336c1ce2e0f5eb349d094c79d8b3ada1e8ae
wagnerSfarias <wagner@universalhost.com.br>
2024-07-25 10:57:23 -0300

    refactor: reanaming variable

--- src/entities/category.entity.ts
+++ src/entities/category.entity.ts
@@ -1,19 +1,17 @@
 type CategoryProps = {
-  id?: string
+  _id?: string
   title: string
   color: string
 }
 
 export class Category {
-  public id?: string
+  public _id?: string
   public title: string
   public color: string
 
-  constructor({ id, title, color }: CategoryProps) {
-    this.id = id
+  constructor({ _id, title, color }: CategoryProps) {
+    this._id = _id
     this.title = title
     this.color = color.toUpperCase()
   }
 }
-
-const catgeory = new Category({ title: 'Primeiro teste', color: '#fff000' })
