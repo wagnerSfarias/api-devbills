f790d8eabbc3ceb13e01139b884e1f0df9731054
wagnerSfarias <wagner@universalhost.com.br>
2024-07-25 10:59:50 -0300

    feat: first commit from category repository

---
+++ src/database/repositories/categories.repository.ts
@@ -0,0 +1,18 @@
+import { Category } from '../../entities/category.entity'
+import { CategoryModel } from '../schemas/category.schema'
+
+export class CategoriesRepository {
+  constructor(private model: typeof CategoryModel) {}
+
+  async create({ title, color }: Category): Promise<Category> {
+    const createdCategory = await this.model.create({ title, color })
+
+    return createdCategory.toObject<Category>()
+  }
+
+  async findByTitle(title: string): Promise<Category | undefined> {
+    const category = await this.model.findOne({ title })
+
+    return category?.toObject<Category>()
+  }
+}
