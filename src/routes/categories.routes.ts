d2a1291c7c2d87734340cdd285785ed08061b63e
wagnerSfarias <wagner@universalhost.com.br>
2024-07-26 10:25:21 -0300

    feat: inserting parameters validator

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
 
 import { CategoriesController } from '../controllers/categories.controloller'
+import { createCategorySchema } from '../dtos/categories.dto'
+import { ParamsType, validator } from '../middleware/validator.middleware'
 
 export const categoriesRoutes = Router()
 
 const controller = new CategoriesController()
 
-categoriesRoutes.post('/', controller.create)
+categoriesRoutes.post(
+  '/',
+  validator({ schema: createCategorySchema, type: ParamsType.BODY }),
+  controller.create,
+)
