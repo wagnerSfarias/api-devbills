35c197a2bc318d5541d6d2694cd3be2b488febe0
wagnerSfarias <wagner@universalhost.com.br>
2024-07-26 09:47:16 -0300

    feat: inserting middleware

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import express, { json } from 'express'
 import 'dotenv/config'
 
 import { setupMongo } from './database'
+import { errorHandler } from './middleware/error-handler.middleware'
 import { routes } from './routes'
 
 setupMongo().then(() => {
   const app = express()
   app.use(json())
   app.use(routes)
+  app.use(errorHandler)
 
   app.listen(3333, () => console.log('🚀 Appp is running at port 3333!'))
 })
