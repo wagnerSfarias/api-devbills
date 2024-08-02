49967afdcc61317bdaee1de3f17c72f4ef69d3b9
wagnerSfarias <wagner@universalhost.com.br>
2024-08-01 22:22:31 -0300

    feat: adding function for category list

--- src/database/repositories/categories.repository.ts
+++ src/database/repositories/categories.repository.ts
@@ -15,4 +15,18 @@ export class CategoriesRepository {
 
     return category?.toObject<Category>()
   }
+
+  async findById(id: string): Promise<Category | undefined> {
+    const category = await this.model.findById({ id })
+
+    return category?.toObject<Category>()
+  }
+
+  async index(): Promise<Category[]> {
+    const categories = await this.model.find()
+
+    const categoriesMap = categories.map((item) => item.toObject<Category>())
+
+    return categoriesMap
+  }
 }
