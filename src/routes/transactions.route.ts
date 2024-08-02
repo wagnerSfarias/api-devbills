82d44f6d58fa76587f46af23406129b733d3adf7
wagnerSfarias <wagner@universalhost.com.br>
2024-08-02 18:36:07 -0300

    feat: creating transaction route

---
+++ src/routes/transactions.route.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+
+import { TransactionsController } from '../controllers/transactions.controller'
+import { createTransactionSchema } from '../dtos/transactions.dto'
+import { TransactionFactory } from '../factories/transactions.factory'
+import { ParamsType, validator } from '../middleware/validator.middleware'
+
+export const transactionsRoutes = Router()
+
+const controller = new TransactionsController(
+  TransactionFactory.getServiceInstance(),
+)
+
+transactionsRoutes.post(
+  '/',
+  validator({ schema: createTransactionSchema, type: ParamsType.BODY }),
+  controller.create,
+)
