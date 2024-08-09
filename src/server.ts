13956d5199550871d2096e297df0ee125b9a3c3c
wagnerSfarias <wagner@universalhost.com.br>
2024-08-09 18:57:09 -0300

    chore : config cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import express, { json } from 'express'
 import 'dotenv/config'
+import cors from 'cors'
+import express, { json } from 'express'
 
 import { setupMongo } from './database'
 import { errorHandler } from './middleware/error-handler.middleware'
@@ -7,6 +8,12 @@ import { routes } from './routes'
 
 setupMongo().then(() => {
   const app = express()
+
+  app.use(
+    cors({
+      origin: process.env.FRONT_URL,
+    }),
+  )
   app.use(json())
   app.use(routes)
   app.use(errorHandler)
